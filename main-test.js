045d17d43df22f8c7355562f1d7c1fe733a46819
victorma17 <victorma17@users.noreply.github.com>
2023-07-24 18:07:11 +0200

    Initial commit

---
+++ main-test.js
@@ -0,0 +1,2 @@
+obj3 = {"numero":123,"fecha":"2021-12- 31T23:00:00.000Z","cliente":{"nombre":"Juan","apellido":"Perez"},"items":[{"descripcion":" Monitor 20\"","precio":300},{"descripcion":"Teclado","precio":20}]}
+console.log(JSON.stringify(obj3, null, 4))
