f19880ad70590d99f19a52f2e13e75c9f4f40788
victorma17 <victorma17@users.noreply.github.com>
2023-07-27 17:01:51 +0200

    Update main-test.js

--- main-test.js
+++ main-test.js
@@ -1,7 +1,6 @@
 obj3 = {"numero":123,"fecha":"2021-12- 31T23:00:00.000Z","cliente":{"nombre":"Juan","apellido":"Perez"},"items":[{"descripcion":" Monitor 20\"","precio":300},{"descripcion":"Teclado","precio":20}]}
 // console.log(JSON.stringify(obj3, null, 3))
 
-
 const edades = [20, 25, 30, 35];
 
 function mediaEdades (edades) {
