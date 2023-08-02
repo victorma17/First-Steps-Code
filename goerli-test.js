1ea56356d5d69e65f13ef4094720d8b0fce72cd9
victorma17 <victorma17@users.noreply.github.com>
2023-08-02 19:00:54 +0200

    bug fixed

--- goerli-test.js
+++ goerli-test.js
@@ -1,10 +1,10 @@
-var Web3 = require("web3")
+let {Web3} = require("web3")
 // provider
 var testnet = "https://eth-goerli.g.alchemy.com/v2/4oCX73Qmleo65zFnQkT-LK8BfWAwXTKT"
 // public account
 var walletAddress = "0x1157466D2Df9D52672e8a62E5e346e8d38B68b84"
 // conexión
-const web3 = new Web3(new Web3.providers.HttpProvider(testnet));
+let web3 = new Web3(new Web3.providers.HttpProvider(testnet));
 // obtenemos el balance
 web3.eth.getBalance(walletAddress).then(i => {
     // convertimos a ether
