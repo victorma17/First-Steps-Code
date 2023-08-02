982b490e0e3263277796d41761ecb487683fca7c
victorma17 <victorma17@users.noreply.github.com>
2023-08-02 19:15:17 +0200

    Update goerli-test.js

--- goerli-test.js
+++ goerli-test.js
@@ -1,14 +1,14 @@
-let {Web3} = require("web3")
+var {Web3} = require("web3")
 // provider
 var testnet = "https://eth-goerli.g.alchemy.com/v2/4oCX73Qmleo65zFnQkT-LK8BfWAwXTKT"
 // public account
 var walletAddress = "0x1157466D2Df9D52672e8a62E5e346e8d38B68b84"
 // conexión
-let web3 = new Web3(new Web3.providers.HttpProvider(testnet));
+var web3 = new Web3(new Web3.providers.HttpProvider(testnet));
 // obtenemos el balance
 web3.eth.getBalance(walletAddress).then(i => {
     // convertimos a ether
     const etherValue = Web3.utils.fromWei(i, 'ether');
-console.log(`${etherValue} ether` ) }).catch(e => {
+console.log(`${etherValue} ether ${i}` ) }).catch(e => {
     console.log(e)
 })
