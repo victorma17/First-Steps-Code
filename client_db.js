9f9ef12a3d485688bfcf1c31a0c07298ecac0871
victorma17 <victorma17@users.noreply.github.com>
2023-08-03 10:38:54 +0200

    add module db

---
+++ client_db.js
@@ -0,0 +1,14 @@
+const { Client } = require('pg')
+const client = new Client({
+    host: 'pgadmin.jvh.kfs.es', port: 5433,
+    database: 'postgres', user: 'lector',
+    password: 'lector',
+})
+const query = async () => {
+    await client.connect()
+    const res = await client.query('SELECT $1::text as message’,
+    ['Hello world!']) console.log(res.rows[0].message) // Hello world! await client.end()
+}
+(async () => {
+    await query()
+})()
